c84e36338e59d2638d1e1f60b1e714de1102dd4f
lohith967 <72453060+lohith967@users.noreply.github.com>
2021-09-09 15:58:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-function preload(){
+objects = [];
+status = "";
 
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(280,280);
+  video.hide();
 }
 
-
-function setup(){
-    canvas = createCanvas(400,300);
-    canvas.center()
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
 }
 
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  object_name = document.getElementById("object_name").value;
+}
 
-
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
 function draw(){
-    
+    image(video, 0, 0, 280, 280);
 }
